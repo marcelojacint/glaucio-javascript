2396c9f91b6ef7c942646192137ee4b5c487155f
marcelojacint <marcelojacintoferreirq@gmail.com>
2025-02-14 20:50:02 -0300

    feito numeros de pessoas do genero feminino e finalizando

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,8 @@ public class Main {
         Locale.setDefault(Locale.US);
         Scanner scanner = new Scanner(System.in);
 
-        for (int i = 0; i < 1; i++) {
+        for (int i = 0; i < 15; i++) {
+
             System.out.println("escreva o nome da pessoa:  ");
             String nome = scanner.nextLine();
 
@@ -24,6 +25,7 @@ public class Main {
 
             System.out.println("escreva a altura da pessoa: ");
             double altura = scanner.nextDouble();
+            scanner.nextLine();
 
             Pessoa pessoa = new Pessoa(nome, genero, altura);
             todasPessoas.add(pessoa);
@@ -36,14 +38,15 @@ public class Main {
             if (pessoa.getGenero().equals("feminino")) {
                 femininas.add(pessoa);
             }
-            System.out.println("a maior altura é: " + verificarMaiorAltura(todasPessoas));
-            System.out.println();
-            System.out.println("a media de altura no gênero masculino é: " + mediaAltura(masculinas));
-            System.out.println();
 
         }
-
         scanner.close();
+
+        System.out.println("a maior altura do grupo é: " + verificarMaiorAltura(todasPessoas));
+        System.out.println();
+        System.out.println("a media de altura no gênero masculino é: " + mediaAltura(masculinas));
+        System.out.println();
+        System.out.println("o número de pessoas do gênero feminino é: " + femininas.size());
 
 
     }
