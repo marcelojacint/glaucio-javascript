7cc29e55c9420eac80220d29f5c80c5b3821318c
marcelojacint <marcelojacintoferreirq@gmail.com>
2025-03-28 20:20:53 -0300

    criado atividade 4

---
+++ atividade4/questao.js
@@ -0,0 +1,25 @@
+function calcularTotal(precoUnitario, quantidade) {
+    return precoUnitario * quantidade;
+}
+
+function aplicarDesconto(valorTotal) {
+    if (valorTotal > 100) {
+        return valorTotal * 0.01; 
+    }
+    return valorTotal;
+}
+
+function exibirResumo(precoUnitario, quantidade) {
+    let valorTotal = calcularTotal(precoUnitario, quantidade);
+    let valorComDesconto = aplicarDesconto(valorTotal);
+
+    console.log("Resumo da Compra:");
+    console.log(`Valor total antes do desconto: R$ ${valorTotal.toFixed(2)}`);
+    console.log(`Valor final com desconto: R$ ${valorComDesconto.toFixed(2)}`);
+}
+
+let preco = parseFloat(prompt("Digite o preço unitário do produto:"));
+let quantidade = parseInt(prompt("Digite a quantidade comprada:"));
+
+exibirResumo(preco, quantidade);
+
