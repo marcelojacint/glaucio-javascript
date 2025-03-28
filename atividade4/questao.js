2cd291bdab719a9c75eb63361b6e84c709f9caf3
marcelojacint <marcelojacintoferreirq@gmail.com>
2025-03-28 20:33:51 -0300

    questao 4 refatorado a saida

--- atividade4/questao.js
+++ atividade4/questao.js
@@ -4,7 +4,7 @@ function calcularTotal(precoUnitario, quantidade) {
 
 function aplicarDesconto(valorTotal) {
     if (valorTotal > 100) {
-        return valorTotal * 0.01; 
+        return valorTotal * 0.9; 
     }
     return valorTotal;
 }
@@ -12,10 +12,15 @@ function aplicarDesconto(valorTotal) {
 function exibirResumo(precoUnitario, quantidade) {
     let valorTotal = calcularTotal(precoUnitario, quantidade);
     let valorComDesconto = aplicarDesconto(valorTotal);
-
-    console.log("Resumo da Compra:");
-    console.log(`Valor total antes do desconto: R$ ${valorTotal.toFixed(2)}`);
-    console.log(`Valor final com desconto: R$ ${valorComDesconto.toFixed(2)}`);
+    if (valorTotal > 100) {
+        console.log("Resumo da Compra:");
+        console.log(`Valor total antes do desconto: R$ ${valorTotal.toFixed(2)}`);
+        console.log(`Valor final com desconto: R$ ${valorComDesconto.toFixed(2)}`);
+    }else{
+        console.log("Resumo da Compra:");
+        console.log(`Valor total sem desconto: R$ ${valorTotal.toFixed(2)}`);
+    }
+    
 }
 
 let preco = parseFloat(prompt("Digite o preço unitário do produto:"));
